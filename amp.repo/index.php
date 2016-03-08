498f29e3a5f42e2dc0e1ef82f12f83e4212ef45b
secret_online <me+github@secretonline.co>
2016-03-09 09:02:18 +1300

    Added Countdown

--- amp.repo/index.php
+++ amp.repo/index.php
@@ -8,9 +8,28 @@
     <link rel="canonical" href="https://repo.nmsdb.info" />
     <link rel="icon" sizes="196x196" href="https://repo.nmsdb.info/res/icon.png">
     <style amp-boilerplate>body{-webkit-animation:-amp-start 8s steps(1,end) 0s 1 normal both;-moz-animation:-amp-start 8s steps(1,end) 0s 1 normal both;-ms-animation:-amp-start 8s steps(1,end) 0s 1 normal both;animation:-amp-start 8s steps(1,end) 0s 1 normal both}@-webkit-keyframes -amp-start{from{visibility:hidden}to{visibility:visible}}@-moz-keyframes -amp-start{from{visibility:hidden}to{visibility:visible}}@-ms-keyframes -amp-start{from{visibility:hidden}to{visibility:visible}}@-o-keyframes -amp-start{from{visibility:hidden}to{visibility:visible}}@keyframes -amp-start{from{visibility:hidden}to{visibility:visible}}</style><noscript><style amp-boilerplate>body{-webkit-animation:none;-moz-animation:none;-ms-animation:none;animation:none}</style></noscript>
+    <style media="screen">
+
+    </style>
     <script async src="https://cdn.ampproject.org/v0.js"></script>
   </head>
   <body>
-    <h1>WIP</h1>
+    <header>
+      <a href="/">
+        <h1 class="title">NMS Information Repository</h1>
+      </a>
+      <div class="button-container">
+        <button type="button" class="desktop">
+          <img height="30" width="30" src="res/desktop.svg" alt="Desktop" />
+        </button>
+      </div>
+    </header>
+    <main>
+    </main>
   </body>
 </html>
+
+
+<?php
+  $info = file_get_contents("../repo/data/info.json");
+ ?>
